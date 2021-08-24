fbdb0010778839e044f94882cd9b42fe94136efd
mart-anthony-stark <martsalazar0@gmail.com>
2021-08-24 14:10:47 -0700

    2 files modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar">
+    <i class="fa fa-home" aria-hidden="true" @click="$emit('navigate', 'home')"></i>
+    <i class="fa fa-user" aria-hidden="true" @click="$emit('navigate', 'profile')"></i>
+    <i class="fa fa-code" aria-hidden="true" @click="$emit('navigate', 'code')"></i>
+    <i class="fa fa-pencil-square-o" aria-hidden="true" @click="$emit('navigate', 'edit')"></i>
+  </nav>
+</template>
+<script>
+export default{
+  name: 'Header',
+  
+}
+
+</script>
